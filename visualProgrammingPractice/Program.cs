75fdb36408eb5db88675e895f86841db862d06a4
Ali Naqi <alyy.naqi80@gmail.com>
2016-03-20 21:51:41 +0500

    visual programming practice

---
+++ visualProgrammingPractice/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visualProgrammingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int? x=null;
+            //x = 2;
+            //Console.WriteLine(x);
+            //Console.Read();
+
+            //string str1 = "Hello world";
+            //string str2;
+            //Console.WriteLine(str1);
+            //str2 =(string) str1.Clone();
+            //Console.WriteLine(str2);
+            //Console.Read();
+
+
+            //string str1 = "Hello world";
+            //string str2 = "Hello world";
+            //string result;
+            //result = String.Concat(str1, str2);
+            //Console.WriteLine(result);
+            //Console.Read();
+
+            //string str = "hello";
+            //string str1 = "hello";
+            //Console.WriteLine(str.CompareTo(str1));
+            //Console.Read();
+
+
+            //string[] s = { "hello ", "and ", "welcome ", "to ",
+            //            "this ", "demo! " };
+            //Console.WriteLine(string.Concat(s));
+            //Console.Read();
+
+            //string str = "Hello";
+            //string str1 = "ll";
+            //Console.WriteLine(str.Contains(str1));
+            //Console.Read();
+
+            //string str = "Hello";
+            //char[] destinantion = {'T', 'h', 'e', ' ', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ',
+            //    'a', 'r', 'r', 'a', 'y'};
+            //str.CopyTo(2, destinantion, 4, 2);
+            //Console.WriteLine(destinantion);
+            //Console.Read();
+
+            //string str1 = "hello";
+            //string str2 = "llo";
+            //Console.WriteLine(str1.Insert(3,str2));
+            //Console.Read();
+
+            //string[] str = { "abc", "def", "ghi" };
+            //string[] result;
+            //Console.WriteLine(result.Join(" ",str));
+
+
+
+            //string str1 = "I m doing csharp practice";
+            //string[] str2 = str1.Split(' ');
+            //Console.WriteLine(str2);
+            //foreach (char s in str1)
+
+            //{
+            //     Console.WriteLine(s);
+            //}
+            //Console.ReadLine();
+
+            //string str1= "cat\r\ndog\r\nanimal\r\nperson";
+            //string[] lines = str1.Split('\r', '\n');
+            //foreach(string s in lines)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //Console.ReadLine();
+
+            //string str = "al is well";
+            //string str1 = "al";
+            //Console.WriteLine(str.StartsWith(str1));
+            //Console.ReadLine();
+
+            //string str = "ALI is learning csharp";
+            //int start = str.IndexOf("is");
+            //int end = str.IndexOf("csharp");
+            //int length = end - start;
+            //Console.WriteLine(str.Substring(start,length));
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
